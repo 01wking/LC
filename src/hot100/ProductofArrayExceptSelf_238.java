4f1adf021655339b34d59ebed7dc4b1f46ea4a71
wph <wPhui4G@2407>
2026-07-14 14:53:51 +0800

    lc238，不要忘记初始化答案数组

--- src/hot100/ProductofArrayExceptSelf_238.java
+++ src/hot100/ProductofArrayExceptSelf_238.java
@@ -1,5 +1,7 @@
 package src.hot100;
 
+import java.util.Arrays;
+
 public class ProductofArrayExceptSelf_238 {
     public static void main(String[] args) {
         int [] nums = {1,2,3,4};
@@ -8,19 +10,18 @@ public class ProductofArrayExceptSelf_238 {
     public static int[] productExceptSelf(int[] nums) {
         int n = nums.length;
         int [] ans = new int [n];
-        ans[0] = 1;
-        ans[n - 1] = 1;
-        int num = 1;
+        Arrays.fill(ans,1);
+        int cur = nums[0];
         for(int i = 1; i < n;i++){
-            num *= nums[i - 1];
-            ans[i] = num;
-        }
-        num = 1;
-        for(int j = n - 2; j >= 0;j--){
-            num *= nums[j + 1];
-            ans[j] *= num;
+            ans[i] = cur;
+            cur *= nums[i];
         }
 
+        cur = nums[n - 1];
+        for(int i = n - 2;i >= 0;i--){
+            ans[i] *= cur;
+            cur *= nums[i];
+        }
         return ans;
     }
 
